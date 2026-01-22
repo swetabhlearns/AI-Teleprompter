a17e8ff7938aff5d1f70b72e064821dea495cb2f
Swetabh Kumar <swetabh.learn@gmail.com>
2026-01-22 21:24:52 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.jsx'
 
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PostHogProvider client={posthog}>
       <App />
+      <SpeedInsights />
     </PostHogProvider>
   </StrictMode>,
 )
